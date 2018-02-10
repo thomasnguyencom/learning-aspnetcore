35bef8e010c26a2d8b20e438a83b88671c614773
thomasnguyencom <contact@thomasnguyen.com>
2018-02-10 13:08:47 -0600

    update namespace to Omgtitb.Learning.AspNetCore

--- source/WebApplication/Program.cs
+++ source/WebApplication/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication1
+namespace Omgtitb.Learning.AspNetCore
 {
     public class Program
     {
