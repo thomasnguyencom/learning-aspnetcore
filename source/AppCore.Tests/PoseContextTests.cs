43a79c1b2f478461f2832e7c69c6415bd5cf7b00
thomasnguyencom <contact@thomasnguyen.com>
2018-02-17 09:27:30 -0600

    Prepare for PoseContextTests - no idea how

---
+++ source/AppCore.Tests/PoseContextTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Omgtitb.Learning.AspNetCore.AppModel;
+using System;
+
+namespace Omgtitb.Learning.AspNetCore.AppCore.Tests
+{
+    [TestClass]
+    public class PoseContextTests
+    {
+        // TODO I don't know how to do this
+    }
+}
