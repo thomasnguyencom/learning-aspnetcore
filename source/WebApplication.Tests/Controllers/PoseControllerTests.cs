6853d0c2ac2b3623d0e1bc8ed3eb3ea617d2b576
thomasnguyencom <contact@thomasnguyen.com>
2018-02-18 11:15:20 -0600

    Prepare for PoseControllerTests

---
+++ source/WebApplication.Tests/Controllers/PoseControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Omgtitb.Learning.AspNetCore.WebApp.Tests.Controllers
+{
+    public class PoseControllerTests
+    {
+        /*
+         *
+        // Arrange
+        var mockRepo = new Mock<IBrainstormSessionRepository>();
+        mockRepo.Setup(repo => repo.ListAsync()).Returns(Task.FromResult(GetTestSessions()));
+        var controller = new HomeController(mockRepo.Object);
+
+        // Act
+        var result = await controller.Index();
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+            viewResult.ViewData.Model);
+        Assert.Equal(2, model.Count());
+        *
+        */
+    }
+}
