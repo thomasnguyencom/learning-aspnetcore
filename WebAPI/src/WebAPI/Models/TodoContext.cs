54922c8c2c550c1c2c73e9a51edf855dc63f5f68
thomasnguyencom <contact@thomasnguyen.com>
2020-10-16 21:32:01 -0500

    Add the TodoContext database context

---
+++ WebAPI/src/WebAPI/Models/TodoContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tag-name")]
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
